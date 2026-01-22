c082bbde0f9767c392af436b98c7a604851ce3bf
mifumi323 <mifumi323@gmail.com>
2026-01-22 09:06:53 +0900

    Itemクラス追加
    #2

---
+++ FLaunch2/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using LiteDB;
+using System;
+
+namespace FLaunch2.Models
+{
+    public class Item
+    {
+        [BsonId]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string DisplayName { get; set; } = string.Empty;
+        public string FilePath { get; set; } = string.Empty;
+        public string WorkingDirectory { get; set; } = string.Empty;
+        public double Score { get; set; } = 0.0;
+        public DateTimeOffset LastExecuted { get; set; }
+        public string Arguments { get; set; } = string.Empty;
+        public string Comment { get; set; } = string.Empty;
+        public string[] Tags { get; set; } = [];
+    }
+}
