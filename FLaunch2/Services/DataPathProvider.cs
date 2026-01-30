f0b5ba49de46647412cc1655af00fd9dec886bf1
mifumi323 <mifumi323@gmail.com>
2026-01-30 20:48:52 +0900

    DataPathProvider追加
    #2

---
+++ FLaunch2/Services/DataPathProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace FLaunch2.Services;
+
+/// <summary>
+/// アプリケーションのデータファイルパスを提供するサービス
+/// </summary>
+public class DataPathProvider
+{
+    private const string AppFolderName = "FLaunch2";
+    private const string DatabaseFileName = "items.db";
+
+    /// <summary>
+    /// データベースファイルのパスを取得します
+    /// </summary>
+    public static string DatabasePath => Path.Combine(AppDataFolder, DatabaseFileName);
+
+    /// <summary>
+    /// アプリケーションデータフォルダのパスを取得します
+    /// </summary>
+    public static string AppDataFolder
+    {
+        get
+        {
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var appFolder = Path.Combine(appDataPath, AppFolderName);
+
+            if (!Directory.Exists(appFolder))
+            {
+                Directory.CreateDirectory(appFolder);
+            }
+
+            return appFolder;
+        }
+    }
+}
